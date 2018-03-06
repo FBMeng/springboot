b40335b92984f9a357a564f2a191f074656e92b4
FBMeng <mengfanbingyx@163.com>
2018-03-06 22:17:43 +0800

    springboot+mybatis

---
+++ testboot/src/main/java/person/bing/testboot/user/dao/java/UserMapping.java
@@ -0,0 +1,9 @@
+package person.bing.testboot.user.dao.java;
+
+import java.util.List;
+
+import person.bing.testboot.user.bean.UserModel;
+
+public interface UserMapping {
+	public List<UserModel> getUser();
+}
