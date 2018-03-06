fd01d4c1f3b17d664b8be2947c5066ef8c6cb04b
FBMeng <mengfanbingyx@163.com>
2018-03-06 21:26:30 +0800

    Revert "springboot"
    This reverts commit e67757fac93cd1f793c827c65c51d746cac2839a.

--- testboot/src/main/java/person/bing/testboot/user/dao/java/UserMapping.java
+++
@@ -1,9 +0,0 @@
-package person.bing.testboot.user.dao.java;
-
-import java.util.List;
-
-import person.bing.testboot.user.bean.UserModel;
-
-public interface UserMapping {
-	public List<UserModel> getUser();
-}
