e67757fac93cd1f793c827c65c51d746cac2839a
FBMeng <mengfanbingyx@163.com>
2018-03-06 21:23:22 +0800

    springboot

---
+++ testboot/src/test/java/person/bing/testboot/TestbootApplicationTests.java
@@ -0,0 +1,16 @@
+package person.bing.testboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
